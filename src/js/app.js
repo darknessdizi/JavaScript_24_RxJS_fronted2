f6b79eb93b90d8279c237c36b664df14743ca02b
Remezov Dmitriy <darknessdizi@gmail.com>
2024-05-13 02:01:38 +0700

    Устранены мелкие недочеты

--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,6 @@ import Widget from './Widget';
 
 const conteiner = document.querySelector('.conteiner-posts');
 const url = 'https://javascript-24-rxjs-backend2.onrender.com';
+
 const widget = new Widget(conteiner, url);
 widget.init();
